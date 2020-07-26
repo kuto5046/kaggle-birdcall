b6bad39415c2efd07878d7eefc86b5636727b573
kyohei <h27.kubb@gmail.com>
2020-07-26 18:34:41 +0900

    [add] base script file

---
+++ src/configuration.py
@@ -0,0 +1,148 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import sklearn.model_selection as sms
+
+import dataset  # src
+
+from pathlib import Path
+
+from criterion import ResNetLoss  # noqa
+from transforms import (get_waveform_transforms,
+                        get_spectrogram_transforms)
+
+
+def get_device(device: str):
+    return torch.device(device)
+
+
+def get_optimizer(model: nn.Module, config: dict):
+    optimizer_config = config["optimizer"]
+    optimizer_name = optimizer_config.get("name")
+
+    return optim.__getattribute__(optimizer_name)(model.parameters(),
+                                                  **optimizer_config["params"])
+
+
+def get_scheduler(optimizer, config: dict):
+    scheduler_config = config["scheduler"]
+    scheduler_name = scheduler_config.get("name")
+
+    if scheduler_name is None:
+        return
+    else:
+        return optim.lr_scheduler.__getattribute__(scheduler_name)(
+            optimizer, **scheduler_config["params"])
+
+
+def get_criterion(config: dict):
+    loss_config = config["loss"]
+    loss_name = loss_config["name"]
+    loss_params = {} if loss_config.get("params") is None else loss_config.get(
+        "params")
+
+    if hasattr(nn, loss_name):
+        criterion = nn.__getattribute__(loss_name)(**loss_params)
+    else:
+        criterion_cls = globals().get(loss_name)
+        if criterion_cls is not None:
+            criterion = criterion_cls(**loss_params)
+        else:
+            raise NotImplementedError
+
+    return criterion
+
+
+# TODO 返り値の挙動
+def get_split(config: dict):
+    split_config = config["split"]
+    name = split_config["name"]
+
+    return sms.__getattribute__(name)(**split_config["params"])
+
+
+def get_metadata(config: dict):
+    data_config = config["data"]
+    with open(data_config["train_skip"]) as f:
+        skip_rows = f.readlines()
+
+    train = pd.read_csv(data_config["train_df_path"])
+    audio_path = Path(data_config["train_audio_path"])
+
+    for row in skip_rows:
+        row = row.replace("\n", "")
+        ebird_code = row.split("/")[1]
+        filename = row.split("/")[2]
+        train = train[~((train["ebird_code"] == ebird_code) &
+                        (train["filename"] == filename))]
+        train = train.reset_index(drop=True)
+    return train, audio_path
+
+
+# resampleされたメタデータを取得(tawata's dataset)
+def get_resampled_metadata(config: dict):
+    data_config = config["data"]
+    tmp_list = []  # dfに変換するための一時格納リスト
+
+    #5つのresample datasetから音声データファイルをiterationし,ebird_name,file名, file_pathをdfに変換
+    # 音声データを読み込んでいるというよりかはメタデータを読み込んでいる
+    for i, audio_d in enumerate(data_config["train_resample_audio_path"]):
+        audio_d = Path(audio_d)
+        tmp_train = pd.read_csv(audio_d / "train_mod.csv")
+
+        if not audio_d.exists():
+            continue
+        for ebird_d in audio_d.iterdir():
+            if ebird_d.is_file():
+                continue
+            for wav_f in ebird_d.iterdir():
+                tmp_list.append([ebird_d.name, wav_f.name, wav_f.as_posix()])
+        
+        # 各データセットのtrain_mod.csvファイルを結合
+        if i == 0:
+            train = tmp_train.copy()
+        else:
+            train = pd.concat([train, tmp_train])
+
+    train_wav_path_exist = pd.DataFrame(
+        tmp_list, columns=["ebird_code", "resampled_filename", "file_path"])
+
+    del tmp_list  # 不要なのでメモリ節約のため削除
+
+    # dfのデータにmerge
+    train_all = pd.merge(
+        train, train_wav_path_exist, on=["ebird_code", "resampled_filename"], how="inner")
+
+    return  train_all
+
+
+# trainとvalid別々で与えられる
+def get_loader(df: pd.DataFrame,
+               # datadir: Path,
+               config: dict,
+               phase: str):
+    dataset_config = config["dataset"]
+    
+    if dataset_config["name"] == "SpectrogramDataset":
+        waveform_transforms = get_waveform_transforms(config)
+        spectrogram_transforms = get_spectrogram_transforms(config)
+        melspectrogram_parameters = dataset_config["params"]
+        loader_config = config["loader"][phase]
+
+        datasets = dataset.SpectrogramDataset(
+            df,
+            # datadir=datadir,
+            img_size=dataset_config["img_size"],
+            waveform_transforms=waveform_transforms,
+            spectrogram_transforms=spectrogram_transforms,
+            melspectrogram_parameters=melspectrogram_parameters)
+    else:
+        raise NotImplementedError
+
+    loader = data.DataLoader(datasets, **loader_config)
+    return loader
+
+
+ 
---
+++ src/train.py
@@ -0,0 +1,81 @@
+import warnings
+
+# src
+import callbacks as clb
+import configuration as C
+import models as models
+import utils as utils
+
+from pathlib import Path
+
+from catalyst.dl import SupervisedRunner
+
+
+def run():
+    warnings.filterwarnings("ignore")
+
+    args = utils.get_parser().parse_args()  # コマンドライン引数で参照するyamlファイルを指定
+    config = utils.load_config(args.config)
+
+    global_params = config["globals"]
+
+    # outputディレクトリの設定
+    output_dir = Path(global_params["output_dir"])
+    output_dir.mkdir(exist_ok=True, parents=True)
+    logger = utils.get_logger(output_dir / "output.log")  # log結果を格納
+
+    utils.set_seed(global_params["seed"])  # seedの固定
+    device = C.get_device(global_params["device"])  # CPU or GPU
+
+    # df, datadir = C.get_metadata(config)  # original meta dataの取得
+    df = C.get_resampled_metadata(config)  # resample meta dataの取得
+    splitter = C.get_split(config)  # CV
+
+    for i, (trn_idx, val_idx) in enumerate(splitter.split(df, y=df["ebird_code"])):
+        if i not in global_params["folds"]:  # 0出なければとばす(1fold-cvを意味する)
+            continue
+
+        logger.info("=" * 20)
+        logger.info(f"Fold {i}")
+        logger.info("=" * 20)
+
+        trn_df = df.loc[trn_idx, :].reset_index(drop=True)
+        val_df = df.loc[val_idx, :].reset_index(drop=True)
+        
+        # train/valid２種類のdata loaderをdict型で作成
+        loaders = {
+            phase: C.get_loader(df_, config, phase)
+            for df_, phase in zip([trn_df, val_df], ["train", "valid"])
+        }
+
+        # configファイルの情報をもとにそれぞれのフレームを作成
+        model = models.get_model(config).to(device)
+        criterion = C.get_criterion(config).to(device)
+        optimizer = C.get_optimizer(model, config)
+        scheduler = C.get_scheduler(optimizer, config)
+        callbacks = clb.get_callbacks(config)
+
+        # catalystのクラスに渡す
+        runner = SupervisedRunner(
+            device=device,
+            input_key=global_params["input_key"],
+            input_target_key=global_params["input_target_key"])
+
+        runner.train(
+            model=model,
+            criterion=criterion,
+            loaders=loaders,
+            optimizer=optimizer,
+            scheduler=scheduler,
+            num_epochs=global_params["num_epochs"],
+            verbose=True,
+            logdir=output_dir / f"fold{i}",
+            callbacks=callbacks,
+            main_metric=global_params["main_metric"],
+            minimize_metric=global_params["minimize_metric"])
+
+
+if __name__ == "__main__":
+    run()
+
+
